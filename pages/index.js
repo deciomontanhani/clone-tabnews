211aa378fdcd3bfc99f3f0ee6ca22f03588a78b5
decio.montanhani <decio.montanhani@ifood.com.br>
2025-04-03 20:56:07 -0300

    nova mensagem de teste

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 function Home() {
   return (
     <div>
-      <h1>Hello, world!</h1>
-      <p>This is a simple Next.js app.</p>
+      <h1>Parabéns choviskinha!!</h1>
+      <p>Se você viu isso, eu mereço um beijinho.</p>
     </div>
   );
 }
