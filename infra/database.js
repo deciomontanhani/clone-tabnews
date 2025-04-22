55ebe14027e1070915733933d5457ada110bc5d8
deciomontanhani <deciomontanhani@gmail.com>
2025-04-22 02:02:56 -0300

    add connections log

--- infra/database.js
+++ infra/database.js
@@ -8,6 +8,13 @@ async function query(queryObject) {
     port: process.env.POSTGRES_PORT,
     database: process.env.POSTGRES_DB,
   });
+  console.log("Credenciais do Postgres:", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
 
   try {
     await client.connect();
